b9ad0cdddf17230a39f44aa476bc9ac819d08e34
Raghav N <ragnr@amazon.com>
2019-11-08 21:35:27 -0800

    working version :-)

--- junior/j4-favourite-times.cc
+++ junior/j4-favourite-times.cc
@@ -1,50 +1,102 @@
 #include <iostream>
 #include <cassert>
-#include <unordered_map>
+#include <vector>
 using namespace std;
 
 class Solution {
 private:
-  unordered_map<int,int> seqMap;
+  vector<int> seqVector;
 private:
+  bool isSeq(int a, int b, int c, int d) {
+    if (a == 1 && b > 2) return false;
+    if (a == 0) {
+        if (b == c && c == d) return true;
+        if (c - b == d - c) return true;
+    } else {
+      if (a == b && b == c && c == d) return true;
+      if ((b - a == c - b) && (c - b == d - c)) return true;
+    }
+    return false;
+  }
+
   void generateArithmeticSequences() {
+    int nSeq = 0;
     int a = 0, b = 0, c = 0, d = 0;
     for (a = 0; a < 2; ++a) {
-      for (b = 1; b < 3; ++b) {
+      for (b = 1; b < 10; ++b) {
         for (c = 0; c < 6; ++c) {
           for (d = 0; d < 10; ++d) {
-            if (a == 0) {
-                if (b == c == d) {
-                  
-                }
-            } else {
-              if (a == b == c == d) {
-
-              } else if (a < b < c < d) {
-
-              } else if (a > b > c > d) {
-
+            if (isSeq(a, b, c, d) == true) {
+              nSeq += 1;
+              int min = (a * 10 + b) * 60 + (c * 10) + d;
+              if (a == 1 && b == 2) {
+                min = c * 10 + d;
               }
+              cout << "seq= " << a << b << ":" << c << d << " minutes=" << min << " nSeq=" << nSeq << endl;
+              seqVector.push_back(min);
             }
           }
         }
       }
     }
   }
-public:
-  int numTimes(int D) {
-    cout << "Hello, World";
-    return 0;
+
+  int numTimesHelper(int D) {
+    int nTimes = 0;
+    for (int i = 0; i < seqVector.size(); ++i) {
+      if (seqVector[i] > D) {
+        break;
+      }
+      nTimes += 1;
+    }
+    return nTimes;
   }
 
+public:
+  int numTimes(int D) {
+    generateArithmeticSequences();
+    sort(seqVector.begin(), seqVector.end());
+    return numTimesHelper(D);
+  }
 };
 
 void test0() {
+  Solution s;
+  int n = s.numTimes(33);
+  assert(n == 0);
+}
+
+void test1() {
   Solution s;
   int n = s.numTimes(34);
   assert(n == 1);
 }
 
+void test2() {
+  Solution s;
+  int n = s.numTimes(180);
+  assert(n == 11);
+}
+
+void test3() {
+  Solution s;
+  int n = s.numTimes(671);
+  cout << "test3=" << n << endl;
+  assert(n == 31);
+}
+
+void test4() {
+  Solution s;
+  int n = s.numTimes(6710);
+  cout << "test4=" << n << endl;
+  assert(n == 31);
+}
+
 int main() {
   test0();
+  test1();
+  test2();
+  test3();
+  test4();
+  return 0;
 }
