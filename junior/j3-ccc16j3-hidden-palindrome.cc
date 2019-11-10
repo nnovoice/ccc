dc6e1e64fa6083b08038a3b47f440478c6ee5280
Raghav N <ragnr@amazon.com>
2019-11-09 16:08:39 -0800

    AC with bruteforce

--- junior/j3-ccc16j3-hidden-palindrome.cc
+++ junior/j3-ccc16j3-hidden-palindrome.cc
@@ -7,41 +7,39 @@ using namespace std;
 
 class Solution {
 private:
-  void printPalindrome(int i, int j, string& s) {
+  void printString(int i, int j, string& s) {
     for (; i <= j; ++i) {
-      cout << s[i];
+      //cout << s[i];
     }
-    cout << endl;
+    //cout << endl;
   }
   bool isPalindrome(int i, int j, string& s) {
     if (i > j) return false;
-
-    for (int k = i, l = j; k != l; k++, --l) {
+    //cout << "isPalindrome=?" << i << ":" << j << " ";
+    printString(i, j, s);
+    for (int k = i, l = j; k < l; k++, --l) {
       if (s[k] != s[l]) return false;
     }
 
-    printPalindrome(i, j, s);
+    //cout << "isPalindrome= :-) ";
+    printString(i, j, s);
     return true;
   }
 public:
   int longestPalindrome(string& s) {
+    int count = 0;
     int maxLen = 1;
-    int i = 0, j = s.length() - 1;
-    for (int i = 0; i < j; ++i, --j) {
+
+    for (int i = 0; i < s.length(); ++i) {
+      for (int j = s.length() - 1; i < j; --j) {
         // if a longer string was found, no need to search more
         if (maxLen > (j - i + 1)) break;
-
+        //cout << count << " ";
+        count += 1;
         if (isPalindrome(i, j, s)) {
           maxLen = max(maxLen, j - i + 1);
-        } else {
-          if (isPalindrome(i + 1, j, s)) {
-            maxLen = max(maxLen, j - i);
-          } else {
-            if (isPalindrome (i, j - 1, s)) {
-              maxLen = max(maxLen, j - i);
-            }
-          }
         }
+      }
     }
     return maxLen;
   }
@@ -50,7 +48,7 @@ public:
 int run(string& s) {
   Solution sol;
   int len = sol.longestPalindrome(s);
-  cout << s << ":=" << len << endl;
+  //cout << s << ":=" << len << endl;
   return (len);
 }
 void t0() {
@@ -78,13 +76,23 @@ void t4() {
   int n = run(s);
   assert(n == 1);
 }
+void t5() {
+  string s = "abcdefghijklmnopqrstuvwxyz0123456789dcba";
+  int n = run(s);
+  assert(n == 1);
+}
 
 int main() {
-  t0();
-  t1();
-  t2();
-  t3();
-  t4();
-  //t5();
+  // t0();
+  // t1();
+  // t2();
+  // t3();
+  // t4();
+  // t5();
+
+  string s;
+  cin >> s;
+  Solution sol;
+  cout << sol.longestPalindrome(s);
   return 0;
 }
